cd2a6ab643b8c7cde95bd295f1ce3a00d0fa1896
Allwyen <allwyen27@gmail.com>
2019-08-20 13:57:26 +0530

    employee

---
+++ app.js
@@ -0,0 +1,76 @@
+const Express = require("express");
+const Mongoose = require('mongoose');
+
+var request = require('request');
+var bodyParser = require('body-parser');
+
+var app = new Express();
+
+app.set('view engine','ejs'); 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+Mongoose.connect("mongodb://localhost:27017/employeedb");
+
+const EmployeeModel= Mongoose.model("employeedetail",{
+    ename:String,
+    edesign:String,
+    esalary:String
+});
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.get('/addemployee',(req,res)=>{
+    res.render('addemployee');
+}); 
+
+app.post('/read',(req,res)=>{
+    //var items=req.body;
+    //res.render('read',{item:items});
+
+    var employee = new EmployeeModel(req.body);
+    var result = employee.save((error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send("<script>alert('Employee Successfully Inserted')</script>");
+        }
+    });
+
+});
+
+app.get('/employeeall',(req,res)=>{
+
+    var result = EmployeeModel.find((error,data)=>{
+        if(error)
+        {
+            throw error;
+            res.send(error);
+        }
+        else
+        {
+            res.send(data);
+        }
+    });
+});
+
+const APIurl = "http://localhost:3456/employeeall";
+
+app.get('/viewemployee',(req,res)=>{
+
+    request(APIurl,(error,response,body)=>{
+        var data = JSON.parse(body);
+        res.render('viewemployee',{data:data});
+    });
+});
+
+app.listen(process.env.PORT || 3456,()=>{
+    console.log("Server running on port::3456...");
+});  //we can assign any number.3000 is commonly used.
